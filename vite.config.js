64c58f958105aeecf21cf9b3189da0ba9de92efd
Nikao777 <>
2024-09-19 04:51:24 -0300

    Eu novamente kkkkk

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://nikao777.github.io/Fake-Twiter/'
+  base: 'https://github.com/Nikao777/Fake-Twiter'
 })
